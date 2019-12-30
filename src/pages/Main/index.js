ef15eb8708cc217148a35c8de79311bb8c718ce7
Joao Lucas <jlmelosouza18@gmail.com>
2019-12-30 15:38:26 -0300

    feat: save repository in localStorage

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -11,6 +11,24 @@ export default class Main extends Component {
     loading: false,
   };
 
+  // Carregar os dados do localStorage
+  componentDidMount() {
+    const repositories = localStorage.getItem('repositories');
+
+    if (repositories) {
+      this.setState({ repositories: JSON.parse(repositories) });
+    }
+  }
+
+  // Salvar os dados do localStorage
+  componentDidUpdate(prevProps, prevState) {
+    const { repositories } = this.state;
+
+    if (prevState.repositories !== repositories) {
+      localStorage.setItem('repositories', JSON.stringify(repositories));
+    }
+  }
+
   handleChangeText = e => {
     this.setState({ newRepo: e.target.value });
   };
